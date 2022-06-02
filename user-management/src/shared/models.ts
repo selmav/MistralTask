41390af269e6a2aa26754284b17fcfc1ad32d9eb
selmav <37186840+selmav@users.noreply.github.com>
2022-06-03 00:36:02 +0200

    Create Angular app. Added user list and grid components, service (#1)

---
+++ user-management/src/shared/models.ts
@@ -0,0 +1,21 @@
+export interface User {
+    firstName: string;
+    lastName: string;
+    username: string;
+    password: string;
+    email: string;
+    status: string;
+}
+
+export interface Permission {
+    code: string;
+    description: string;
+}
+
+// Request/Response models
+
+export interface UserList {
+    page: number;
+    totalPages: number;
+    results: User[]
+}
