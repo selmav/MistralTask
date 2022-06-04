3baee7d81e53b6c07ca910a3b268a5ff2f02b7a1
selmav <svucijak1@etf.unsa.ba>
2022-06-04 18:37:25 +0200

    Recreate project with Angular CLI 14

--- UserManagement/src/app/app-routing.module.ts
+++ UserManagement/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from 'src/user-list/user-list.component';
-
 
 const routes: Routes = [
   {
